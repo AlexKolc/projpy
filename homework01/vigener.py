d7a3c72c593669efe45483b78e68fe46b7ea724c
alexkolc <alex_kolc@inbox.ru>
2017-10-14 01:30:03 +0300

    Реализована функция decrypt_caesar()

--- homework01/vigener.py
+++ homework01/vigener.py
@@ -36,5 +36,21 @@ def decrypt_vigenere(ciphertext, keyword):
     >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
     'ATTACKATDAWN'
     """
-    # PUT YOUR CODE HERE
+    plaintext = ""
+    for i in range(len(ciphertext)):
+        shift = ord(keyword[i % len(keyword)])
+        if ciphertext[i] >= 'A' and ciphertext[i] <= 'Z':
+            shift -= ord('A')
+            if ord(ciphertext[i]) - shift >= ord('A'):
+                plaintext += chr(ord(ciphertext[i]) - shift)
+            else:
+                plaintext += chr(ord('Z') - ord('A') + ord(ciphertext[i]) - shift + 1)
+        elif ciphertext[i] >= 'a' and ciphertext[i] <= 'z':
+            shift -= ord('a')
+            if ord(ciphertext[i]) - shift >= ord('a'):
+                plaintext += chr(ord(ciphertext[i]) - shift)
+            else:
+                plaintext += chr(ord('z') - ord('a') + ord(ciphertext[i]) - shift + 1)
+        else:
+            plaintext += ciphertext[i]
     return plaintext
